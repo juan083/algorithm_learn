04c8f4de84dc8dc45db7efec09651fc534280ec5
juan083 <juan083@163.com>
2020-11-28 22:16:40 +0800

    n queens

---
+++ src/php/dfs/n_queens.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @description  n_queens.php
+ * @author:      juan083@163.com
+ * @createtime:  2020-11-28 22:12
+ */
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return String[][]
+     */
+
+    private $_col = [];
+
+    private $_pie = [];
+
+    private $_na = [];
+
+    private $_ret = [];
+
+
+    function solveNQueens($n) {
+
+    }
+
+    function dfs($n, $target) {
+        if ($target >= $n) {
+
+        }
+    }
+}
